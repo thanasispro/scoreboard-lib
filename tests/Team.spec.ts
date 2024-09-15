64604aad32b2f41c358c87604faf7a3bd0a9c766
Thanasis <protopapas.dim.ath@outlook.com>
2024-09-15 23:55:53 +0300

    test: can add a score at Team class

--- tests/Team.spec.ts
+++ tests/Team.spec.ts
@@ -1,9 +1,19 @@
 import { Team } from '../src/models/Team';
 
 describe('Team Class', () => {
+  let team: Team;
+
+  beforeEach(() => {
+    team = new Team('Olympiacos');
+  })
+
   it('creates an instance with the given name and a default score', () => {
-    const team = new Team('Olympiacos');
     expect(team.getName()).toBe('Olympiacos');
     expect(team.getScore()).toBe(0);
   });
+
+  it('can set a score for the Team', () => {
+    team.setScore(1);
+    expect(team.getScore()).toBe(1);
+ })
 });
