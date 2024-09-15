74a47dad12a61cc1144fce2e2bdf3bd80129ae49
Thanasis <protopapas.dim.ath@outlook.com>
2024-09-16 00:24:06 +0300

    test: add score validation at Team class

--- tests/Team.spec.ts
+++ tests/Team.spec.ts
@@ -1,19 +1,27 @@
-import { Team } from '../src/models/Team';
+import { Team } from "../src/models/Team";
 
-describe('Team Class', () => {
+describe("Team Class", () => {
   let team: Team;
 
   beforeEach(() => {
-    team = new Team('Olympiacos');
-  })
+    team = new Team("Olympiacos");
+  });
 
-  it('creates an instance with the given name and a default score', () => {
-    expect(team.getName()).toBe('Olympiacos');
+  it("creates an instance with the given name and a default score", () => {
+    expect(team.getName()).toBe("Olympiacos");
     expect(team.getScore()).toBe(0);
   });
 
-  it('can set a score for the Team', () => {
+  it("can set a score for the Team", () => {
     team.setScore(1);
     expect(team.getScore()).toBe(1);
- })
+  });
+
+  it('throws an error when score is a negative number', () => {
+    expect(() => team.setScore(-5)).toThrow('Invalid score -5: Score must be a non-negative integer.');
+  });
+
+  it('throws an error when score is a non-integer number', () => {
+    expect(() => team.setScore(2.3)).toThrow('Invalid score 2.3: Score must be a non-negative integer.');
+  });
 });
