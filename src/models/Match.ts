8532fe87999821d7294d84d74c51669b85043117
Thanasis <protopapas.dim.ath@outlook.com>
2024-09-16 09:22:07 +0300

    feat: add Match id to trach game

--- src/models/Match.ts
+++ src/models/Match.ts
@@ -1,12 +1,20 @@
 import { Team } from "./Team";
+import { v4 as uuidv4 } from 'uuid';
+
 
 export class Match {
   private homeTeam: Team;
   private awayTeam: Team;
+  private id: string;
 
   constructor(homeTeamName: string, awayTeamName: string) {
     this.homeTeam = new Team(homeTeamName);
     this.awayTeam = new Team(awayTeamName);
+    this.id = uuidv4();
+  }
+
+  getId() {
+    return this.id;
   }
 
   print() {
