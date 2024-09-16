5d3e651156e252f2e104569f25923eedb393de59
Thanasis <protopapas.dim.ath@outlook.com>
2024-09-16 09:14:28 +0300

    test: Match class creation

---
+++ tests/Match.spec.ts
@@ -0,0 +1,13 @@
+import { Match } from "../src/models/Match";
+
+describe("Match class", () => {
+  let match: Match;
+
+  beforeEach(() => {
+    match = new Match("Olympiacos", "Rosenborg");
+  });
+
+  it("creates an instance with the given names and a default scores", () => {
+    expect(match.print()).toBe("Olympiacos 0 - Rosenborg 0");
+  });
+});
