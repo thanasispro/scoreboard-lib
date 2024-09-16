d2d6bd843c366368066deeb546538a0317ec1f12
Thanasis <protopapas.dim.ath@outlook.com>
2024-09-16 23:37:56 +0300

    test: add team name validation at Match class

--- tests/Match.spec.ts
+++ tests/Match.spec.ts
@@ -1,19 +1,47 @@
 import { Match } from "../src/models/Match";
 
-describe("Match class", () => {
-  let match: Match;
+describe("Match class:", () => {
+  describe("Creation and methods:", () => {
+    let match: Match;
 
-  beforeEach(() => {
-    match = new Match("Olympiacos", "Rosenborg");
+    beforeEach(() => {
+      match = new Match("Olympiacos", "Rosenborg");
+    });
+
+    it("creates an instance with the given names and a default scores", () => {
+      expect(match.print()).toBe("Olympiacos 0 - Rosenborg 0");
+      expect(match.getId()).toBeDefined();
+    });
+
+    it("can update scores to the teams", () => {
+      match.updateScore(0, 1);
+      expect(match.print()).toBe("Olympiacos 0 - Rosenborg 1");
+    });
   });
+  
+  describe("Validation:", () => {
+    it("should throw an error if the home and away teams have the same name", () => {
+      expect(() => {
+        new Match("Olympiacos", "Olympiacos");
+      }).toThrow("Home and away teams cannot have the same name.");
+    });
 
-  it("creates an instance with the given names and a default scores", () => {
-    expect(match.print()).toBe("Olympiacos 0 - Rosenborg 0");
-    expect(match.getId()).toBeDefined();
-  });
+    it("should throw an error for case-insensitive identical names", () => {
+      expect(() => {
+        new Match("Olympiacos", "olympiacos");
+      }).toThrow("Home and away teams cannot have the same name.");
+    });
 
-  it("can update scores to the teams", () => {
-    match.updateScore(0, 1);
-    expect(match.print()).toBe("Olympiacos 0 - Rosenborg 1");
+    it("should throw an error if the home team name is an empty string", () => {
+      expect(() => {
+        new Match("", "Rosenborg");
+      }).toThrow("Team names cannot be empty.");
+    });
+
+    it("should throw an error if the away team name is an empty string", () => {
+      expect(() => {
+        new Match("Olympiacos", "");
+      }).toThrow("Team names cannot be empty.");
+    });
   });
 });
