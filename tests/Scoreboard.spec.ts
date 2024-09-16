60c4601e1e9306452dea859d4f90737e7892deeb
Thanasis <protopapas.dim.ath@outlook.com>
2024-09-16 09:58:37 +0300

    test: initialize Scoreboard class

---
+++ tests/Scoreboard.spec.ts
@@ -0,0 +1,13 @@
+import { Scoreboard } from '../src/services/Scoreboard';
+import { Match } from '../src/models/Match';
+
+describe('Scoreboard', () => {
+  it('should add a match to the scoreboard', () => {
+    const scoreboard = new Scoreboard();
+    const matchId = scoreboard.addMatch('Olympiacos', 'Rosenborg');
+
+    const match = scoreboard.getMatch(matchId);
+    expect(match).toBeInstanceOf(Match);
+    expect(match.print()).toBe('Olympiacos 0 - Rosenborg 0');
+  });
+});
